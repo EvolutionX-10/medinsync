b2a51511943f35caeae4ea483e6fac57a6b6f960
Mefisto04 <mayurkawale0000@gmail.com>
2024-02-10 05:10:35 +0530

    Delete unused login and register files, update User and Hospital models

---
+++ app/api/get-hospital/route.js
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+
+export async function POST(req) {
+	const data = await req.json();
+	const prisma = new PrismaClient();
+	const user = await prisma.hospital.findUnique({
+		where: {
+			name: data.name,
+			gstNo: data.gstNo,
+		},
+	});
+	if (user) {
+		return Response.json({
+			status: 200,
+			body: JSON.stringify(user),
+		});
+	} else {
+		return Response.json({
+			status: 404,
+			body: JSON.stringify({ message: "user not found" }),
+		});
+	}
+}
