c528f5e55b94f5d42f26c6afe441f9d5f196e410
EvolutionX <85353424+EvolutionX-10@users.noreply.github.com>
2024-02-11 02:26:55 +0530

    feat: add font styling

--- app/page.jsx
+++ app/page.jsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 import Image from "next/image";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { figtree } from "@/app/fonts";
 export default function Page() {
 	return (
 		<div className="ml-6 lg:ml-20 mr-6 lg:mr-20">
 			<Navbar />
 
 			{/* Text1 */}
-			
 
 			<div className="flex flex-col items-center  justify-center lg:mt-40 gap-4">
 				<p className="lg:text-6xl text-xl font-bold text-black">Your</p>
@@ -19,8 +19,12 @@ export default function Page() {
 
 			<div className="flex items-center justify-center mt-10">
 				<Link href="/about">
-					<button className="rounded-full ml-0 bg-[#0075FF] px-16 py-3 text-white h-[5rem] w-[20rem] text-4xl">
-						About
+					<button
+						className={
+							"rounded-full ml-0 bg-[#0075FF] px-16 py-3 text-white h-[5rem] w-[20rem] text-4xl " + figtree.className
+						}
+					>
+						bout
 					</button>
 				</Link>
 			</div>
